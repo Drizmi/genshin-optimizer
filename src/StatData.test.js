3daf5038d5110846e11c2606aa35d5e03e659684
lantua <lantua@users.noreply.github.com>
2021-03-19 11:28:59 -0400

    Added Swirl test

--- src/StatData.test.js
+++ src/StatData.test.js
@@ -1,5 +1,6 @@
 import { PreprocessFormulas, Formulas, StatData } from "./StatData"
 import { FormulaText } from "./Stat"
+import { GetDependencies } from "./StatDependency"
 
 describe(`Testing StatData`, () => {
   describe(`PreprocessFormulas()`, () => {
@@ -49,6 +50,40 @@ describe(`Testing StatData`, () => {
       //checks for development
       expect(Object.keys(Formulas)).toEqual(expect.arrayContaining(Object.keys(FormulaText)))
       expect(Object.keys(StatData)).toEqual(expect.arrayContaining(Object.keys(Formulas)))
+    })
+
+    // Discord ID: 822486382450049024
+    // Discord Handle: Derpy#2132
+    test("should correctly compute Swirl dmgs", () => {
+      const stats = {
+        characterLevel: 70,
+        enemyLevel: 87,
+      }
+      const { initialStats, formula } = PreprocessFormulas(GetDependencies({}, ["hydro_swirl_hit"]), stats)
+      
+      // Sucrose 70/80 vs. Ruin Guard 87
+      // Hydro Swirl DMG 753, EM 229, no set / bonus
+      // Hydro/Cryo/Electro Swirl DMG 805, EM 269, Viridescent 2-piece
+      // Hydro/Cryo/Electro Swirl DMG 1327, EM 269, Viridescent 4-piece
+      // Hydro/Cryo/Electro Swirl DMG 1084, EM 434, Viridescent 2-piece
+      // Hydro/Cryo/Electro Swirl DMG 1578, EM 434, Viridescent 4-piece
+
+      let tmp
+      tmp = { ...initialStats, eleMas: 229 }
+      formula(tmp); expect(tmp.hydro_swirl_hit).toApproximate(753)
+      
+      tmp = { ...initialStats, eleMas: 269, anemo_dmg_: 15 } // 2VV
+      formula(tmp); expect(tmp.hydro_swirl_hit).toApproximate(805)
+
+      tmp = { ...initialStats, eleMas: 269, anemo_dmg_: 15 , swirl_dmg_: 60, hydro_enemyRes_: 40 } // 4VV
+      formula(tmp); expect(tmp.hydro_swirl_hit).toApproximate(1327)
+
+      tmp = { ...initialStats, eleMas: 434, anemo_dmg_: 15 } // 2VV
+      formula(tmp); expect(tmp.hydro_swirl_hit).toApproximate(1084)
+      console.log(tmp.hydro_swirl_hit)
+
+      tmp = { ...initialStats, eleMas: 434, anemo_dmg_: 15 , swirl_dmg_: 60, hydro_enemyRes_: 40 } // 4VV
+      formula(tmp); expect(tmp.hydro_swirl_hit).toApproximate(1578)
     })
   })
 })
